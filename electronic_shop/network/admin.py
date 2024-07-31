d98133d9a9ead9316e8523b7791a4229e89f0ff5
gremvanek <gremvanek@gmail.com>
2024-07-31 19:51:35 +0500

    Команда для создания superuser. Добавил правки.

--- electronic_shop/network/admin.py
+++ electronic_shop/network/admin.py
@@ -1,26 +1,22 @@
+from traceback import format_tb
 from django.contrib import admin
 from .models import NetworkElement
 
-
 # Admin action для очистки задолженности
-@admin.action(description="Очистить задолженность перед поставщиком")
+@admin.action(description='Очистить задолженность перед поставщиком')
 def clear_debt(modeladmin, request, queryset):
     queryset.update(debt=0)
 
-
 @admin.register(NetworkElement)
 class NetworkElementAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "email",
-        "country",
-        "city",
-        "street",
-        "house_number",
-        "supplier",
-        "debt",
-        "created_at",
-    )
-    list_filter = ("city",)
+    list_display = ('name', 'email', 'country', 'city', 'street', 'house_number', 'supplier_link', 'debt', 'created_at')
+    list_filter = ('city',)
     actions = [clear_debt]
-    search_fields = ("name", "city")
+    search_fields = ('name', 'city')
+    readonly_fields = ('supplier_link',)
+
+    def supplier_link(self, obj):
+        if obj.supplier:
+            return format_tb('<a href="{}">{}</a>', admin.utils.quote(obj.supplier.pk), obj.supplier.name)
+        return '-'
+    supplier_link.short_description = 'Поставщик'
--- electronic_shop/network/views.py
+++ electronic_shop/network/views.py
@@ -3,9 +3,14 @@ from .models import NetworkElement
 from .serializers import NetworkElementSerializer
 from rest_framework.permissions import IsAuthenticated
 
-
 class NetworkElementViewSet(viewsets.ModelViewSet):
     queryset = NetworkElement.objects.all()
     serializer_class = NetworkElementSerializer
     permission_classes = [IsAuthenticated]
-    filterset_fields = ["country"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        country = self.request.query_params.get('country')
+        if country:
+            queryset = queryset.filter(country=country)
+        return queryset
