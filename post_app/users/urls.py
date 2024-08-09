8524c719300ee7794e2257a2505c10874c1eb01b
gremvanek <gremvanek@gmail.com>
2024-08-09 13:18:44 +0500

    Правки для добавления ауентификации и применение миграций.

--- post_app/users/urls.py
+++ post_app/users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomUserViewSet
+from .views import CustomUserViewSet, LogoutView
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r"users", CustomUserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("auth/token/login/", obtain_auth_token, name="token_login"),
+    path("auth/token/logout/", LogoutView.as_view(), name="token_logout"),
 ]
